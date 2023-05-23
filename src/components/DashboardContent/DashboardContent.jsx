cea76ea0da94aec28fc7eb03b61005ee9f69f253
lucqaz01 <Lucassobczak3@gmail.com>
2023-05-22 23:47:56 -0400

    Added navigation

--- src/components/DashboardContent/DashboardContent.jsx
+++ src/components/DashboardContent/DashboardContent.jsx
@@ -1,12 +1,15 @@
 import {LegacyCard, EmptyState} from '@shopify/polaris';
 import emptyState from '../../assets/images/emptystate.png'
 
-export const DashboardContent = () => {
+export const DashboardContent = ({handleContinueClick}) => {
     return(
         <LegacyCard sectioned>
             <EmptyState
             heading="Welcome to Shopify!"
-            action={{content: 'Continue'}}
+            action={{
+                content: 'Continue',
+                onAction: handleContinueClick
+            }}
             image={emptyState}
             >
                 <p>Let's create your first website together with our Shopify Bot</p>
--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,10 +1,17 @@
+import { useNavigate } from "react-router-dom";
 import { DashboardContent } from "../../components/DashboardContent/DashboardContent";
 // import { Nav } from "../../components/Nav/Nav";
 
 export const LandingPage = () => {
+
+  const navigate = useNavigate()
+
+  const handleContinueClick = () => {
+    navigate('/step-1')
+  }
   // <Nav />;
   // <SideNav />;
   return(
-    <DashboardContent />
+    <DashboardContent handleContinueClick={handleContinueClick}/>
   )
 };
