0059022e8680a484cf401956fdda0e0dedd4cc32
Evan Cheng <evancheng2014@gmail.com>
2023-05-23 01:22:22 -0700

    Revert "Feature/chat"

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,24 @@
-import { LandingPage } from './pages/LandingPage/LandingPage';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import ChatBox from './components/ChatBot/ChatBot';
+import { LandingPage } from "./pages/LandingPage/LandingPage";
+import { BrowserRouter, Route, Routes  } from "react-router-dom";
 
-function App() {
-    return (
-        <>
-            <ChatBox />
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<LandingPage />} />
-                    {/* <Route path="/step-1" element={} /> */}
-                    {/* <Route path="/step-2" element={} /> */}
-                    {/* <Route path="/step-3" element={} /> */}
-                    {/* <Route path="/step-4" element={} /> */}
-                    {/* <Route path="/step-5" element={} /> */}
-                    {/* <Route path="/step-6" element={} /> */}
-                    {/* <Route path="/themes" element={} /> */}
-                </Routes>
-            </BrowserRouter>
-        </>
-    );
-}
+function App(){
+  return(
+    // <></>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      {/* <Route path="/step-1" element={} /> */}
+      {/* <Route path="/step-2" element={} /> */}
+      {/* <Route path="/step-3" element={} /> */}
+      {/* <Route path="/step-4" element={} /> */}
+      {/* <Route path="/step-5" element={} /> */}
+      {/* <Route path="/step-6" element={} /> */}
+      {/* <Route path="/themes" element={} /> */}
+    </Routes>
+  </BrowserRouter> 
+  )
+  
+
+};
 
 export default App;
