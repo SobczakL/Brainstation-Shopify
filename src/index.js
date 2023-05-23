e408658fa32ed59f3e4401d810cf8570917264ec
lucqaz01 <Lucassobczak3@gmail.com>
2023-05-22 23:35:35 -0400

    Layout finished

--- src/index.js
+++ src/index.js
@@ -2,14 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-
+import translations from '@shopify/polaris/locales/en.json';
 import { AppProvider } from '@shopify/polaris';
+import "@shopify/polaris/build/esm/styles.css";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
-		{/* <AppProvider> */}
+		<AppProvider i18n={translations}>
 			<App />
-		{/* </AppProvider> */}
+		</AppProvider>
 	</React.StrictMode>
 );
--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,6 +1,10 @@
-import { Nav } from "../../components/Nav/Nav";
+import { DashboardContent } from "../../components/DashboardContent/DashboardContent";
+// import { Nav } from "../../components/Nav/Nav";
 
 export const LandingPage = () => {
   // <Nav />;
   // <SideNav />;
+  return(
+    <DashboardContent />
+  )
 };
