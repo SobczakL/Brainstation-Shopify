923f543d2db7500fa3f9e7d0abb9d2cd15d36500
Evan Cheng <evancheng2014@gmail.com>
2023-05-23 20:17:01 -0700

    fix unique key problem

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import { LandingPage } from './pages/LandingPage/LandingPage';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import ChatBox from './components/ChatBot/ChatBot';
-import ChooseTheme from './pages/ChooseTheme/ChooseTheme';
 import { ProcessPage } from './pages/ProcessPage/ProcessPage';
 
 
@@ -15,10 +14,8 @@ function App() {
                     <Route path="/" element={<LandingPage />} />
                     {/* <Route path="/step-1" element={} /> */}
                     {/* <Route path="/step-2" element={} /> */}
-                    <Route path="/step-3" element={<ChooseTheme />} />
                     <Route path="/process-page" element={<ProcessPage />} />
                     <Route path="/themes" element={''} />
-                    {/* <Route path="/step-3" element={} /> */}
                     {/* <Route path="/step-4" element={} /> */}
                     {/* <Route path="/step-5" element={} /> */}
                     {/* <Route path="/step-6" element={} /> */}
