70be9748bc161729320d2ceaab6e78f4f7baf5d6
Evan Cheng <evancheng2014@gmail.com>
2023-05-24 03:06:07 -0700

    open and close chatbot

--- src/components/ChatBot/ChatBot.jsx
+++ src/components/ChatBot/ChatBot.jsx
@@ -1,22 +1,31 @@
-import React, { useState, useEffect, useRef } from 'react';
-import './chatbot.scss';
+import React, { useState, useEffect, useRef } from "react";
+import "./chatbot.scss";
 
-import { getFeedback } from '../../axios';
-import Message from './Message';
+import { getFeedback } from "../../axios";
+import Message from "./Message";
 
 const ChatBox = props => {
     const msgListRef = useRef(undefined);
-    const [userMsg, setUserMsg] = useState('');
+    const [isOpen, setIsOpen] = useState(true);
+    const [userMsg, setUserMsg] = useState("");
 
     const handleKeyDown = e => {
-        if (e.key === 'Enter' && e.target.value !== '') {
-            props.addMessage(e.target.value, 'User');
-            setUserMsg('');
+        if (e.key === "Enter" && e.target.value !== "") {
+            props.addMessage(e.target.value, "User");
+            setUserMsg("");
         }
     };
 
     const handleMsgChange = e => {
         setUserMsg(e.target.value);
+    };
+
+    const handleClose = () => {
+        setIsOpen(false);
+    };
+
+    const handleOpen = () => {
+        setIsOpen(true);
     };
 
     useEffect(() => {
@@ -32,45 +41,57 @@ const ChatBox = props => {
     }, [props.messages, userMsg]);
 
     useEffect(() => {
-        msgListRef.current.scrollIntoView({ behavior: 'smooth', block: 'end' });
+        if (isOpen)
+            msgListRef.current.scrollIntoView({
+                behavior: "smooth",
+                block: "end",
+            });
     });
 
-    return (
-        <div className="chatbot-frame">
-            <div className="chatbot-box">
-                <div className="chatbot-box__header">
-                    <span className="title">Ada</span>
-                </div>
-                <div className="chatbot-box__body">
-                    <div className="message-container">
-                        <div
-                            className="message-container__list"
-                            ref={msgListRef}
-                        >
-                            {props.messages &&
-                                props.messages.map((msg, i) => {
-                                    return (
-                                        <Message
-                                            key={i}
-                                            sender={msg.sender}
-                                            msg={msg.message}
-                                        />
-                                    );
-                                })}
-                        </div>
+    if (isOpen) {
+        return (
+            <div className="chatbot-frame">
+                <div className="chatbot-box">
+                    <div className="chatbot-box__header" onClick={handleClose}>
+                        <span className="title">Ada</span>
                     </div>
-                    <div className="input">
-                        <input
-                            placeholder="Chat"
-                            value={userMsg}
-                            onChange={e => handleMsgChange(e)}
-                            onKeyDown={e => handleKeyDown(e)}
-                        />
+                    <div className="chatbot-box__body">
+                        <div className="message-container">
+                            <div
+                                className="message-container__list"
+                                ref={msgListRef}
+                            >
+                                {props.messages &&
+                                    props.messages.map((msg, i) => {
+                                        return (
+                                            <Message
+                                                key={i}
+                                                sender={msg.sender}
+                                                msg={msg.message}
+                                            />
+                                        );
+                                    })}
+                            </div>
+                        </div>
+                        <div className="input">
+                            <input
+                                placeholder="Chat"
+                                value={userMsg}
+                                onChange={e => handleMsgChange(e)}
+                                onKeyDown={e => handleKeyDown(e)}
+                            />
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    } else {
+        return (
+            <div className="close" onClick={handleOpen}>
+                <span>Ada</span>
+            </div>
+        );
+    }
 };
 
 export default ChatBox;
